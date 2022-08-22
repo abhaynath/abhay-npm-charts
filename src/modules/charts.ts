c9bda485098d8fcffb2a57602ee3e80ec953a6bb
Abhay Nath <abhaynath88@gmail.com>
2022-08-22 20:42:50 +0530

    documentation added

--- src/modules/charts.ts
+++ src/modules/charts.ts
@@ -1,5 +1,15 @@
+/**
+ * Chart is the container of utilities of methods
+ *
+ * @class Charts
+ */
 class Charts {
-  
+  /**
+   * scales the chart
+   *
+   * @returns {number} returns the aspect ratio
+   * @memberof Charts
+   */
   public scale(): number {
     return 56;
   }
